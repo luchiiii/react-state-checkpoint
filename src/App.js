8602527c7804a61d742b8f7365155afe707b6a35
okwuosaoluchi <okwuosaoluchi@yahoo.com>
2024-09-03 14:39:18 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "John Doe",
+        bio: "Software Engineer",
+        imgSrc: "(link unavailable)",
+        profession: "Developer",
+      },
+      shows: false,
+      lastMounted: new Date(),
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({ lastMounted: new Date() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  toggleShow() {
+    this.setState({ shows: !this.state.shows });
+  }
+
+  render() {
+    const { person, shows, lastMounted } = this.state;
+    const timeSinceMounted = Math.floor((new Date() - lastMounted) / 1000);
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {shows && (
+          <div>
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+        <p>Time since last mounted: {timeSinceMounted} seconds</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Person;
